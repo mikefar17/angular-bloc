4b91113b1f3de3f612b27b9c96fe82deb38884a7
Michael Farias <michaelfarias75@gmail.com>
2017-08-16 09:06:01 -0700

    Checkpoint Angular routing and states: complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,21 @@
-angular.module('blocJams', []);
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true, // disables #! hashbangs
+                requireBase: false // avoid $location error
+        });
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+        });
+    }
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
